82b48544e0fd38dbf24e787b7f0f096b71a11cf6
Marcos Gasparini <103968839+JunynBoy@users.noreply.github.com>
2022-12-15 19:07:30 -0300

    mini att

--- POO_Bytebank/src/Conta.java
+++ POO_Bytebank/src/Conta.java
@@ -22,8 +22,14 @@ public class Conta {
 		this.saldo += valor;
 	}
 	
-	public void saca (double valor) {
-		this.saldo -= valor;
+	public boolean saca (double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 	
 	public void status() {
